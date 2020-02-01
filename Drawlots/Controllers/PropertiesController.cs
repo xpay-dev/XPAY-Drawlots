a26f98d9dd6b5f7f99e3d880356c0163146fdd89
joyreyes123 <joy@xpay.world>
2020-02-01 10:34:31 +0800

    Property Page and controllers added

---
+++ Drawlots/Controllers/PropertiesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Drawlots.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        // GET: Properties
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PropertyDetails()
+        {
+            return View();
+
+        }
+    }
+}
